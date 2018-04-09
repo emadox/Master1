7ed1c2f01d29087fa62b6c53dc67e951ec03b6d6
Ema Dox <ema.alvarez.1990@gmail.com>
2018-04-09 12:28:50 -0300

    Primer Commit

---
+++ Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour {
+
+   public float moveSpeed;
+   public float jumpSpeed = 100f;
+   public bool Saltando;
+   Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Saltando = false;
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-moveSpeed*Time.deltaTime, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(moveSpeed*Time.deltaTime, rb.velocity.y);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.W) && !Saltando)
+        {
+            Saltando = true;
+            rb.velocity = new Vector2(0, jumpSpeed);
+           
+        }
+
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Piso")
+        {
+            Saltando = false;
+        }
+    }
+}
+
+
